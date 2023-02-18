096fab217e552502c684a274bebf9ce0d232c6b5
mgblanco10 <mgblanco1001@gmail.com>
2023-02-18 07:31:05 +0100

    examples typescript

---
+++ examplesBasic/index.ts
@@ -0,0 +1,11 @@
+console.log('hola mundo con Typescript');
+alert('hola mundo con ts');
+
+var valor:string = "5";
+var valor1:number = 3;
+
+var sumar = (val:string, val1:number)=>{
+    console.log('resultado: '+ (val+val1));
+};
+
+sumar(valor, valor1);
