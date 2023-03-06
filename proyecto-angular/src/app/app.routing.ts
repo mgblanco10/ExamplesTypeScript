eeb922b2933c0df929b62f7c918731d7aba61eb8
mgblanco10 <mgblanco1001@gmail.com>
2023-03-06 17:41:45 +0100

    baby steps angular

---
+++ proyecto-angular/src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ErrorComponent } from './components/error/error.component';
+
+const appRoutes : Routes = [
+    {path: '', component:AboutComponent},
+    {path: 'about-me', component:AboutComponent},
+    {path:'projects', component: ProjectsComponent},
+    {path: 'create-Projects', component:CreateComponent},
+    {path: 'contact', component:ContactComponent},
+    {path:'**', component: ErrorComponent }
+];
+
+export const appRoutingProviders: any [] = [];
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes);
