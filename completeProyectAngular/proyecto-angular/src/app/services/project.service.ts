1e23c9f6b4681a1d05535c9fa83c72dc948c61b2
mgblanco10 <mgblanco1001@gmail.com>
2023-03-07 16:34:19 +0100

    progress of angular

--- completeProyectAngular/proyecto-angular/src/app/services/project.service.ts
+++ completeProyectAngular/proyecto-angular/src/app/services/project.service.ts
@@ -16,4 +16,13 @@ export class ProjectService{
     testService(){
         return 'Probando angula servicio';
     }
+
+    saveProject(project: Project): Observable <any>{
+        let params = JSON.stringify(project);
+        let headers = new HttpHeaders().set('Content-type', 'application/json');
+
+        // return this._http.post(this.url+'save-project', params, {headers:headers});
+
+
+    }
 }
